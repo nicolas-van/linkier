31ffd2dbf070235a1d5995b6a40813ba4f1680e2
Nicolas Vanhoren <nicolas.vanhoren.01@gmail.com>
2013-07-17 15:18:45 +0200

    wip

---
+++ app.js
@@ -0,0 +1,24 @@
+
+(function() {
+    var app = {};
+    window.app = app;
+
+    app.writer = function() {
+        $("button").click(function() {
+            var val = $("textarea").val();
+            val = JSON.stringify({body:val})
+            var url = "" + new URI("display.html").hash(val);
+            $("a").attr("href", url);
+        });
+    };
+
+    app.display = function() {
+        var hash = new URI().fragment() || "{}";
+        state = JSON.parse(hash);
+        _.defaults(state, {
+            body: "",
+        });
+        $("body").html(state.body);
+    };
+
+})();
