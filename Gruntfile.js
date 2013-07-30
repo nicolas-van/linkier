a38ab585298c0d9874fd6ff7fa8c8bfada5d57ad
Nicolas Vanhoren <nicolas.vanhoren.01@gmail.com>
2013-07-30 16:32:50 +0200

    Better deployment config

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,20 @@
 module.exports = function(grunt) {
 
+  var jsfiles = [
+    "bower_components/underscore/underscore.js",
+    "bower_components/URIjs/src/URI.js",
+    "bower_components/jquery/jquery.js",
+    "bower_components/bootstrap/bootstrap/js/bootstrap.js",
+    "bower_components/lz-string/libs/lz-string-1.3.0.js",
+    "bower_components/ace/build/src/ace.js",
+    "app.js",
+  ];
+
+  var cssfiles = [
+    "style.css",
+  ];
+
   grunt.initConfig({
-    concat: {
-      options: {
-        separator: ';'
-      },
-      dist: {
-        src: ['app.js'],
-        dest: 'app-concat.js'
-      }
-    },
-    uglify: {
-      dist: {
-        files: {
-          'app.min.js': ['app-concat.js']
-        }
-      }
-    },
     jshint: {
       files: ['app.js'],
     },
@@ -30,23 +28,90 @@ module.exports = function(grunt) {
         }
       }
     },
+    consolidate: {
+      options: {
+        engine: 'handlebars'
+      },
+      dev: {
+        options: {
+          local: {
+            jsfiles: jsfiles,
+            cssfiles: cssfiles,
+          }
+        },
+        files: [{
+          src: 'index.src.html',
+          dest: 'index.html',
+        }],
+      },
+      dist: {
+        options: {
+          local: {
+            jsfiles: ["app.min.js"],
+            cssfiles: ["style.min.css"],
+          }
+        },
+        files: [{
+          src: 'index.src.html',
+          dest: 'index.tmp.html',
+        }],
+      },
+    },
     watch: {
-      files: ['style.less'],
-      tasks: ['less']
+      less: {
+        files: ['style.less'],
+        tasks: ['less']
+      },
+      html: {
+        files: ['index.src.html'],
+        tasks: ['consolidate:dev']
+      },
+    },
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: jsfiles,
+        dest: 'app-concat.js'
+      }
+    },
+    uglify: {
+      dist: {
+        files: {
+          'dist/app.min.js': ['app-concat.js']
+        }
+      }
+    },
+    cssmin: {
+      dist: {
+        files: {
+          'dist/style.min.css': "style.css",
+        }
+      },
+    },
+    htmlmin: {
+      dist: {
+        files: {
+          'dist/index.html': "index.tmp.html",
+        }
+      },
+    },
+    clean: {
+      tmp: {
+        src: ["app-concat.js", "index.tmp.html"]
+      }
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib');
+  grunt.loadNpmTasks('grunt-consolidate');
 
   grunt.registerTask('test', ['jshint']);
 
-  grunt.registerTask('dev', ['less']);
+  grunt.registerTask('dev', ['less', 'consolidate:dev']);
 
-  grunt.registerTask('dist', ['test', 'less', 'concat', 'uglify']);
+  grunt.registerTask('dist', ['test', 'less', 'concat', 'uglify', "cssmin", "consolidate:dist", "htmlmin", "clean:tmp"]);
 
   grunt.registerTask('default', ['dev']);
 
